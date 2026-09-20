3c64f8afdad10d841b919df20b16026d2dfaea34
Muhammedemirzindan <muhammedemirzindan34@gmail.com>
2026-09-20 13:39:25 +0300

    Create windows_devices.py

---
+++ config/windows_devices.py
@@ -0,0 +1,7 @@
+WINDOWS_DEVICES = [
+    {"name": "win7pro",  "ip": "192.168.174.20", "user": "win7pro",         "password": "win7pro"},
+    {"name": "win10pro", "ip": "192.168.174.21", "user": "win10pro",        "password": "win10pro"},
+    {"name": "win10ent", "ip": "192.168.174.22", "user": "win10enterprise", "password": "win10enterprise"},
+    {"name": "win11pro", "ip": "192.168.174.23", "user": "win11proo",       "password": "win11pro"},
+    {"name": "win11ent", "ip": "192.168.174.24", "user": "win11enterprise", "password": "win11enterprise"},
+]
