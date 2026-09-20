5372610ab914bbc520e0b6616c32a4defd151f88
Muhammedemirzindan <muhammedemirzindan34@gmail.com>
2026-09-20 13:38:52 +0300

    Create devices.py

---
+++ config/devices.py
@@ -0,0 +1,7 @@
+LINUX_DEVICES = [
+    {"name": "ubuntu18",   "ip": "192.168.174.10", "user": "ubuntu18",   "password": "ubuntu18"},
+    {"name": "ubuntu22",   "ip": "192.168.174.11", "user": "ubuntu22",   "password": "ubuntu22"},
+    {"name": "ubuntu24",   "ip": "192.168.174.12", "user": "ubuntu24",   "password": "ubuntu24"},
+    {"name": "ubuntu26",   "ip": "192.168.174.13", "user": "ubuntu26",   "password": "ubuntu26"},
+    {"name": "almalinux9", "ip": "192.168.174.14", "user": "almalinux9", "password": "almalinux9"},
+]
