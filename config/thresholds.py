0a28c535c64f365f97f7ae8b0b859ebe0a8f7dbe
Muhammedemirzindan <muhammedemirzindan34@gmail.com>
2026-09-20 13:39:50 +0300

    Create thresholds.py

---
+++ config/thresholds.py
@@ -0,0 +1,18 @@
+THRESHOLDS = {
+    "cpu": {
+        "warning": 85,
+        "critical": 95
+    },
+    "ram": {
+        "warning": 75,
+        "critical": 90
+    },
+    "disk": {
+        "warning": 80,
+        "critical": 90
+    },
+    "ping": {
+        "warning": 22,
+        "critical": 30
+    }
+}
